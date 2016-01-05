b486657859622ca94fe2437665319fb766a0c362
shinya takahashi <s.takahashi313@gmail.com>
2016-01-05 22:15:16 +0900

    Initial commit

---
+++ MemoizedSelector/MemoizedSelector.h
@@ -0,0 +1,19 @@
+//
+//  MemoizedSelector.h
+//  MemoizedSelector
+//
+//  Created by Shinya Takahashi on 2016/01/05.
+//  Copyright © 2016年 Shinya Takahashi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MemoizedSelector.
+FOUNDATION_EXPORT double MemoizedSelectorVersionNumber;
+
+//! Project version string for MemoizedSelector.
+FOUNDATION_EXPORT const unsigned char MemoizedSelectorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MemoizedSelector/PublicHeader.h>
+
+
---
+++ MemoizedSelectorTests/MemoizedSelectorTests.swift
@@ -0,0 +1,36 @@
+//
+//  MemoizedSelectorTests.swift
+//  MemoizedSelectorTests
+//
+//  Created by Shinya Takahashi on 2016/01/05.
+//  Copyright © 2016年 Shinya Takahashi. All rights reserved.
+//
+
+import XCTest
+@testable import MemoizedSelector
+
+class MemoizedSelectorTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
